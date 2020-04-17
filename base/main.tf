9568bdb7446be32cc5096d63ce2a5da1a5aac868
Marko Vuksanovic <mvuksano@thoughtworks.com>
2020-04-16 22:30:26 -0700

    Specify permissions for DP1 and mark sections which belong to each DP.

--- base/main.tf
+++ base/main.tf
@@ -5,16 +5,19 @@ resource "google_project_service" "service" {
   disable_on_destroy = false
 }
 
+# DP1 Service Account
 resource "google_service_account" "service_account-dp-1" {
   account_id   = "dp-1-sa"
   display_name = "Service Account for data product 1"
 }
 
+# DP2 Service account
 resource "google_service_account" "service_account-dp-2" {
   account_id   = "dp-2-sa"
   display_name = "Service Account for data product 2"
 }
 
+# DP1 Output port
 resource "google_bigquery_dataset" "dataset-dp-1" {
   dataset_id                  = "dp1ds"
   friendly_name               = "dp1-dataset"
@@ -26,16 +29,29 @@ resource "google_bigquery_dataset" "dataset-dp-1" {
     role          = "OWNER"
     user_by_email = google_service_account.service_account-dp-1.email
   }
+
+  access {
+    role = "READER"
+    user_by_email = google_service_account.service_account-dp-2.email
+  }
 }
 
+# DP1 Input port
 resource "google_storage_bucket" "dp-1-sb" {
   name          = "dp-1-loading-zone"
   location      = "US"
   force_destroy = true
 }
 
-
+# DP1 SP permissions
 data "google_iam_policy" "dp-1-admin" {
+  binding {
+    role = "roles/storage.objectCreator"
+    members = [
+      "serviceAccount:${google_service_account.service_account-dp-1.email}",
+    ]
+  }
+
   binding {
     role = "roles/storage.admin"
     members = [
@@ -44,8 +60,14 @@ data "google_iam_policy" "dp-1-admin" {
     ]
   }
 }
+
 resource "google_storage_bucket_iam_policy" "dp-1-sb-policy" {
   bucket = google_storage_bucket.dp-1-sb.name
+  policy_data = data.google_iam_policy.dp-1-admin.policy_data
+}
+
+resource "google_storage_bucket_iam_policy" "dp-1-dataflow-temp-sb-policy" {
+  bucket = google_storage_bucket.dp-1-dataflow-temp.name
   policy_data = data.google_iam_policy.dp-1-admin.policy_data
 }
 
@@ -54,16 +76,17 @@ resource "google_project_iam_member" "dp-1-sa-bq-job-user" {
   role    = "roles/bigquery.jobUser"
   member  = "serviceAccount:${google_service_account.service_account-dp-1.email}"
 }
-resource "google_project_iam_member" "dp-2-sa-bq-job-user" {
+
+resource "google_project_iam_member" "dp-1-sa-dataflow-developer" {
   project = var.project 
-  role    = "roles/bigquery.jobUser"
-  member  = "serviceAccount:${google_service_account.service_account-dp-2.email}"
+  role    = "roles/dataflow.developer"
+  member  = "serviceAccount:${google_service_account.service_account-dp-1.email}"
 }
 
-resource "google_storage_bucket" "dp-1-dataflow-temp" {
-  name          = "dp-1-df-temp"
-  location      = "US"
-  force_destroy = true
+resource "google_project_iam_member" "dp-1-sa-compute-viewer" {
+  project = var.project 
+  role    = "roles/compute.viewer"
+  member  = "serviceAccount:${google_service_account.service_account-dp-1.email}"
 }
 
 resource "google_storage_bucket_iam_member" "df-1-dataflow-temp-admin" {
@@ -72,6 +95,19 @@ resource "google_storage_bucket_iam_member" "df-1-dataflow-temp-admin" {
   member = "serviceAccount:64709029339-compute@developer.gserviceaccount.com"
 }
 
+resource "google_project_iam_member" "dp-2-sa-bq-job-user" {
+  role    = "roles/bigquery.jobUser"
+  member  = "serviceAccount:${google_service_account.service_account-dp-2.email}"
+}
+
+# DP1 internals
+resource "google_storage_bucket" "dp-1-dataflow-temp" {
+  name          = "dp-1-df-temp"
+  location      = "US"
+  force_destroy = true
+}
+
+# DP2 output port
 resource "google_bigquery_dataset" "dataset-dp-2" {
   dataset_id                  = "dp2ds"
   friendly_name               = "dp2-dataset"
